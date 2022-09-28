cb454a1a13f4c70a615b85fe5fc85434033e143b
jtherme <jerome.therme@amadeus.com>
2022-09-28 16:59:02 +0200

    Basic blog frontent

---
+++ my-blog/src/pages/ArticlePage.js
@@ -0,0 +1,25 @@
+import { useParams } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
+
+import articles from "./article-content";
+
+const ArticlePage = () => {
+  const params = useParams();
+  const articleId = params.articleId;
+  const article = articles.find(article => article.name === articleId);
+  
+  if(!article){
+    return <NotFoundPage/>
+  }
+  
+  return (
+    <>
+      <h1>{article.title}!</h1>
+      {article.content.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+      ))}
+    </>
+  )
+}
+
+export default ArticlePage;
